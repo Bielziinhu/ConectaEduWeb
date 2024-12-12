39cd5829a0569db0d30446541518311a25b07ddf
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-12-12 15:06:49 -0300

    feat: Conecta tela de login com a API

---
+++ infraestructure/js/login.js
@@ -0,0 +1,49 @@
+// Seleção dos elementos do formulário
+const form = {
+    username: document.querySelector("#username"),
+    password: document.querySelector("#password"),
+    submit: document.querySelector(".btn"), 
+    messages: document.getElementById("form-messages") 
+};
+
+// URL da API de login
+const loginEndpoint = 'http://localhost:8080/conecta-edu/v1/auth';
+
+// Adicionando o evento de clique ao botão de submissão
+form.submit.addEventListener("click", (e) => {
+    e.preventDefault(); // Evita o comportamento padrão do botão
+
+    // Faz a requisição à API
+    fetch(loginEndpoint, {
+        method: "POST",
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: form.username.value,
+            password: form.password.value,
+        }),
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Login falhou. Verifique suas credenciais.");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Login bem-sucedido:", data);
+        alert("Login realizado com sucesso!");
+    
+        // Armazenar o token em localStorage ou sessionStorage
+        localStorage.setItem('authToken', data.token); // Assume 'token' é o nome do campo na resposta JSON
+    
+        // Redireciona o usuário após o login
+        window.location.href = '/pages/admin-escola.html';
+    })
+    
+    .catch((err) => {
+        console.error("Erro no login:", err);
+        alert("Erro ao realizar login. Tente novamente.");
+    });
+});
